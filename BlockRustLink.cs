f4946105482cd9558c6130005ea6d5d4e7087235
Strobez <41481352+Strrobez@users.noreply.github.com>
2022-02-12 01:10:10 +0000

    Update BlockRustLink.cs

--- BlockRustLink.cs
+++ BlockRustLink.cs
@@ -3,7 +3,6 @@ namespace Oxide.Plugins
     [Info("BlockRustLink", "Strrobez", "0.0.1")]
     internal class BlockRustLink : RustPlugin
     {
-
         object OnPlayerChat(BasePlayer player, string message)
         {
             if (player == null) return null;
